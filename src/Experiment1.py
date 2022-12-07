Correct EB mu_hat formula

src/Experiment1.py
@@ -17,10 +17,10 @@ from ExperimentBase import *
 # Two experiments
 
 
-def get_methods_dict(lambda_max=2, cs='betting', comp_cs=None):
+def get_methods_dict(lambda_max=2, cs='Bet', method_suite='default'):
     """Generate the methods dictionary for this experiment."""
     methods_dict = {}
-    if comp_cs is None:
+    if method_suite == 'default':
         methods_dict['propM'] = {
             'method_name': 'propM',
             'use_CV': False,
@@ -33,11 +33,13 @@ def get_methods_dict(lambda_max=2, cs='betting', comp_cs=None):
             'use_CV': False,
             'lambda_max': lambda_max
         }
-        if cs != 'betting':
+        if cs != 'Bet':
             for key in methods_dict:
                 methods_dict[key]['cs'] = cs
     else:
-        for cs in ['Bet', 'Hoef.', 'Emp. Bern.']:
+        cses = ['Hoef.', 'Emp. Bern.'
+                ] + ([] if method_suite == 'comp_cs' else ['Bet'])
+        for cs in cses:
             methods_dict[cs] = {
                 'method_name': 'propM',
                 'cs': cs,
@@ -52,6 +54,7 @@ def get_methods_dict(lambda_max=2, cs='betting', comp_cs=None):
 
 def CSexperiment(M_ranges,
                  f_ranges,
+                 method_suite,
                  N=200,
                  N1=60,
                  lambda_max=2,
@@ -80,7 +83,7 @@ def CSexperiment(M_ranges,
 
     # create the methods dict
     methods_dict = get_methods_dict(lambda_max=lambda_max,
-                                    comp_cs=args.method_suite == 'comp_cs')
+                                    method_suite=method_suite)
     # create the figure information dictionary
     xlabel = r'Sample Size ($n$) '
     if plot_CS_width:
@@ -112,8 +115,10 @@ def CSexperiment(M_ranges,
 
 def HistExperiment1(M_ranges,
                     f_ranges,
+                    method_suite,
                     N=200,
                     N1=100,
+                    lambda_max=2,
                     epsilon=0.05,
                     inv_prop=True,
                     verbose=False,
@@ -132,7 +137,8 @@ def HistExperiment1(M_ranges,
         # assume the original f values are proportional to M
         # so we don't need any modification here
     # generate the dictionary with methods information
-    methods_dict = get_methods_dict()
+    methods_dict = get_methods_dict(lambda_max=lambda_max,
+                                    method_suite=method_suite)
     # generate the dictionary with histogram plotting information
     StoppingTimesDict = getStoppingTimesDistribution(
         methods_dict,
@@ -174,7 +180,7 @@ if __name__ == '__main__':
     #### Set experiment options
     CSExpt = args.mode == 'cs'
     HistExpt = not CSExpt
-    N = 200
+    N = args.N
     N1 = int(np.floor(args.small_prop * N))
     inv_prop = args.f_method == 'inv'
 
@@ -192,6 +198,7 @@ if __name__ == '__main__':
     if CSExpt:
         CSexperiment(M_ranges,
                      f_ranges,
+                     args.method_suite,
                      N=N,
                      N1=N1,
                      lambda_max=lambda_max,
@@ -206,8 +213,10 @@ if __name__ == '__main__':
     if HistExpt:
         HistExperiment1(M_ranges,
                         f_ranges,
+                        args.method_suite,
                         N=N,
                         N1=N1,
+                        lambda_max=lambda_max,
                         epsilon=epsilon,
                         inv_prop=inv_prop,
                         verbose=True,

src/bernstein.py
@@ -65,7 +65,7 @@ def eb_boundary(xs: np.ndarray,
         lambda_den_t = c_sq_t * avg_var_t * pred_inv_sum_t * (t0 / t)
 
     lambda_num = 8 * np.log(1 / alpha)
-    lambda_t = np.minimum(np.sqrt(lambda_num / lambda_den_t), 1 / (1.1 * c_t))
+    lambda_t = np.minimum(np.sqrt(lambda_num / lambda_den_t), 1 / (2 * c_t))
     lambda_sum_t = np.cumsum(lambda_t)
     mu_hat_t = np.cumsum(lambda_t * xs) / lambda_sum_t
 

src/hoeffding.py
@@ -17,7 +17,7 @@ def hoeffding_boundaries(xs,
     inv_sum_t = np.cumsum(1 / ct_sq)
 
     if lambda_strategy is None:
-        lambda_strategy = 'opt_uniform'
+        lambda_strategy = 'approx_uniform'
     if lambda_strategy == 'approx_uniform':
         lambda_den_t = t * np.log(t + 1) * avg_c_sq_t
     elif lambda_strategy == 'approx_fixed':

src/weightedCSsequential.py
@@ -484,12 +484,23 @@ def run_one_expt(M,
                 alpha=alpha,
                 lambda_strategy=lambda_strategy)
         else:  # cs == 'Emp. Bern.':
-            LowerCS, UpperCS = (
-                eb_boundary(np.array(Z_t),
-                            np.array(lbs),
-                            alpha / 2,
-                            lambda_strategy=lambda_strategy), 1. -
-                eb_boundary(np.array(Z_om_t), np.array(lb_oms), alpha / 2))
+            shift_pif = np.append(0, (Pi * f)[I_t][:-1])
+            normal_est = np.cumsum(Z_t + shift_pif) / np.arange(1, N + 1)
+            normal_est = np.append(0.5, normal_est[:-1])
+            rev_shift_pif = np.append(0, (Pi * (1 - f))[I_t][:-1])
+            rev_est = np.cumsum(Z_om_t + rev_shift_pif) / np.arange(1, N + 1)
+            rev_est = np.append(0.5, rev_est[:-1])
+            LowerCS, UpperCS = (eb_boundary(np.array(Z_t),
+                                            np.array(lbs),
+                                            alpha / 2,
+                                            ests=normal_est,
+                                            lambda_strategy=lambda_strategy),
+                                1. -
+                                eb_boundary(np.array(Z_om_t),
+                                            np.array(lb_oms),
+                                            alpha / 2,
+                                            ests=rev_est,
+                                            lambda_strategy=lambda_strategy))
     if logical_CS or intersect:
         LowerCS, UpperCS = predictive_correction1(LowerCS,
                                                   UpperCS,
@@ -509,9 +520,11 @@ def main(A=0.1, use_CV=False):
     N1 = 150
     N2 = N - N1
     f_over_S_range = [1 - A, 1 + A]
-    f_ranges = [[0.4, 0.5], [1e-3, 2 * 1e-3]]
+    #f_ranges = [[0.4, 0.5], [1e-3, 2 * 1e-3]]
+    f_ranges = [[0.4, 0.5], [0.4, 0.5]]
     # M_ranges = [ [1e5, 1e6], [1e2, 1*1e3]],
-    M_ranges = [[1e2, 2e2], [5e2, 8e2]]
+    #M_ranges = [[1e2, 2e2], [5e2, 8e2]]
+    M_ranges = [[1e2, 2e2], [1e2, 2e2]]
     M, f, S = generate_MFS(
         N_vals=(N1, N2),
         N=N,  # total number of transactions = sum(N_vals)
@@ -521,56 +534,42 @@ def main(A=0.1, use_CV=False):
 
     nG = 100
     lambda_max = 2
-    result_propMS = run_one_expt(M,
-                                 f,
-                                 S,
-                                 method_name='propMS',
-                                 lambda_max=lambda_max,
-                                 beta_max=0.5,
-                                 nG=nG,
-                                 use_CV=use_CV,
-                                 f_over_S_range=f_over_S_range,
-                                 alpha=0.05,
-                                 logical_CS=False,
-                                 intersect=False,
-                                 return_payoff=False)
-    grid, _, L1, U1, _, _ = result_propMS
+    fig = plt.figure()
+    ax = plt.gca()
+    cs_method_pairs = [
+        #('Hoef.', 'uniform', 'opt_uniform'),
+        ('Emp. Bern.', 'propM', 'opt_uniform'),
+        #           ('Hoef.', 'uniform', 'approx_uniform'),
+        ('Emp. Bern.', 'propM', 'approx_uniform')
+    ]
+    for cs, method, ls in cs_method_pairs:
+        result_propMS = run_one_expt(M,
+                                     f,
+                                     S,
+                                     cs=cs,
+                                     method_name=method,
+                                     lambda_max=lambda_max,
+                                     beta_max=0.5,
+                                     nG=nG,
+                                     use_CV=use_CV,
+                                     f_over_S_range=f_over_S_range,
+                                     alpha=0.05,
+                                     logical_CS=False,
+                                     intersect=False,
+                                     lambda_strategy=ls,
+                                     return_payoff=False)
+        grid, _, L1, U1, _, _ = result_propMS
 
-    result_propM = run_one_expt(M,
-                                f,
-                                S,
-                                method_name='propM',
-                                lambda_max=lambda_max,
-                                beta_max=0.5,
-                                nG=nG,
-                                use_CV=use_CV,
-                                f_over_S_range=None,
-                                alpha=0.05,
-                                logical_CS=False,
-                                intersect=False,
-                                return_payoff=False)
-    _, _, L2, U2, _, _ = result_propM
-
-    result_unif = run_one_expt(M,
-                               f,
-                               S,
-                               method_name='uniform',
-                               lambda_max=lambda_max,
-                               beta_max=0.5,
-                               nG=nG,
-                               use_CV=use_CV,
-                               f_over_S_range=None,
-                               alpha=0.05,
-                               logical_CS=False,
-                               intersect=False,
-                               return_payoff=False)
-    _, _, L3, U3, _, _ = result_unif
-
-    NN = np.arange(1, N + 1)
-    plt.plot(NN, U1 - L1, label='propMS')
-    plt.plot(NN, U2 - L2, label='propM')
-    plt.plot(NN, U3 - L3, label='uniform')
+        NN = np.arange(1, N + 1)
+        ax.plot(NN, U1 - L1, label=f'{cs} {method} {ls}')
+        # prev_p = ax.plot(NN, U1, label=f'{cs} {method} {ls}')
+        # ax.plot(NN, L1, color=prev_p[0].get_color())
+    ax.axhline(np.sum(M / np.sum(M) * f), linestyle='dashed')
+    ax.set_ylabel('CS width')
+    ax.set_xlabel('$n$')
     plt.legend()
+    fig.tight_layout()
+    fig.savefig('test_results.png', dpi=300)
 
 
 def testCV(A=0.1):
@@ -627,4 +626,5 @@ def testCV(A=0.1):
 
 
 if __name__ == '__main__':
-    testCV(0.9999)
+    #testCV(0.9999)
+    main()